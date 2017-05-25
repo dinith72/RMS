14da5c959671ff5b9e69c54b94509a428912b2bd
Najjaath <najjaath@gmail.com>
2017-05-25 13:27:20 +0530

    Najjaath1

---
+++ ResultManagementSystemKalpa/Registration_Form.designer.cs
@@ -0,0 +1,280 @@
+﻿namespace ResultManagementSystemKalpa
+{
+    partial class Registration_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Registration_Form));
+            this.Stream_ComboBox = new System.Windows.Forms.ComboBox();
+            this.District_ComboBox = new System.Windows.Forms.ComboBox();
+            this.LastName_TextBox = new System.Windows.Forms.TextBox();
+            this.AcadamicYear_ComboBox = new System.Windows.Forms.ComboBox();
+            this.FirstName_TextBox = new System.Windows.Forms.TextBox();
+            this.ExtraCarricularActivities_Label = new System.Windows.Forms.Label();
+            this.Profile_Label = new System.Windows.Forms.Label();
+            this.Profile_PictureBox = new System.Windows.Forms.PictureBox();
+            this.Stream_Label = new System.Windows.Forms.Label();
+            this.ZScore_Label = new System.Windows.Forms.Label();
+            this.District_Label = new System.Windows.Forms.Label();
+            this.AcadamicYear_Label = new System.Windows.Forms.Label();
+            this.LastName_Label = new System.Windows.Forms.Label();
+            this.FirstName_Label = new System.Windows.Forms.Label();
+            this.RagistrationNo_MaskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.RagistrationNo_Label = new System.Windows.Forms.Label();
+            this.ExtraCarricularActivities_TextBox = new System.Windows.Forms.TextBox();
+            this.ZScore_MaskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Profile_PictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Stream_ComboBox
+            // 
+            this.Stream_ComboBox.BackColor = System.Drawing.SystemColors.Info;
+            this.Stream_ComboBox.FormattingEnabled = true;
+            this.Stream_ComboBox.Location = new System.Drawing.Point(162, 404);
+            this.Stream_ComboBox.Name = "Stream_ComboBox";
+            this.Stream_ComboBox.Size = new System.Drawing.Size(205, 21);
+            this.Stream_ComboBox.TabIndex = 77;
+            this.Stream_ComboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox7_SelectedIndexChanged);
+            // 
+            // District_ComboBox
+            // 
+            this.District_ComboBox.BackColor = System.Drawing.SystemColors.Info;
+            this.District_ComboBox.FormattingEnabled = true;
+            this.District_ComboBox.Location = new System.Drawing.Point(163, 308);
+            this.District_ComboBox.Name = "District_ComboBox";
+            this.District_ComboBox.Size = new System.Drawing.Size(205, 21);
+            this.District_ComboBox.TabIndex = 75;
+            // 
+            // LastName_TextBox
+            // 
+            this.LastName_TextBox.BackColor = System.Drawing.SystemColors.Info;
+            this.LastName_TextBox.Location = new System.Drawing.Point(162, 261);
+            this.LastName_TextBox.Name = "LastName_TextBox";
+            this.LastName_TextBox.Size = new System.Drawing.Size(205, 20);
+            this.LastName_TextBox.TabIndex = 74;
+            // 
+            // AcadamicYear_ComboBox
+            // 
+            this.AcadamicYear_ComboBox.BackColor = System.Drawing.SystemColors.Info;
+            this.AcadamicYear_ComboBox.FormattingEnabled = true;
+            this.AcadamicYear_ComboBox.Location = new System.Drawing.Point(162, 355);
+            this.AcadamicYear_ComboBox.Name = "AcadamicYear_ComboBox";
+            this.AcadamicYear_ComboBox.Size = new System.Drawing.Size(205, 21);
+            this.AcadamicYear_ComboBox.TabIndex = 73;
+            // 
+            // FirstName_TextBox
+            // 
+            this.FirstName_TextBox.BackColor = System.Drawing.SystemColors.Info;
+            this.FirstName_TextBox.Location = new System.Drawing.Point(162, 214);
+            this.FirstName_TextBox.Name = "FirstName_TextBox";
+            this.FirstName_TextBox.Size = new System.Drawing.Size(205, 20);
+            this.FirstName_TextBox.TabIndex = 72;
+            // 
+            // ExtraCarricularActivities_Label
+            // 
+            this.ExtraCarricularActivities_Label.AutoSize = true;
+            this.ExtraCarricularActivities_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExtraCarricularActivities_Label.Location = new System.Drawing.Point(470, 246);
+            this.ExtraCarricularActivities_Label.Name = "ExtraCarricularActivities_Label";
+            this.ExtraCarricularActivities_Label.Size = new System.Drawing.Size(113, 32);
+            this.ExtraCarricularActivities_Label.TabIndex = 71;
+            this.ExtraCarricularActivities_Label.Text = "Extra Curricular\r\n    Activities";
+            // 
+            // Profile_Label
+            // 
+            this.Profile_Label.AutoSize = true;
+            this.Profile_Label.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Profile_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Profile_Label.Location = new System.Drawing.Point(469, 179);
+            this.Profile_Label.Name = "Profile_Label";
+            this.Profile_Label.Size = new System.Drawing.Size(107, 15);
+            this.Profile_Label.TabIndex = 70;
+            this.Profile_Label.Text = "Add Profile Picture";
+            // 
+            // Profile_PictureBox
+            // 
+            this.Profile_PictureBox.BackColor = System.Drawing.Color.White;
+            this.Profile_PictureBox.Image = ((System.Drawing.Image)(resources.GetObject("Profile_PictureBox.Image")));
+            this.Profile_PictureBox.Location = new System.Drawing.Point(449, 20);
+            this.Profile_PictureBox.Name = "Profile_PictureBox";
+            this.Profile_PictureBox.Size = new System.Drawing.Size(147, 156);
+            this.Profile_PictureBox.TabIndex = 69;
+            this.Profile_PictureBox.TabStop = false;
+            // 
+            // Stream_Label
+            // 
+            this.Stream_Label.AutoSize = true;
+            this.Stream_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Stream_Label.Location = new System.Drawing.Point(20, 405);
+            this.Stream_Label.Name = "Stream_Label";
+            this.Stream_Label.Size = new System.Drawing.Size(84, 16);
+            this.Stream_Label.TabIndex = 68;
+            this.Stream_Label.Text = "A/L Stream";
+            // 
+            // ZScore_Label
+            // 
+            this.ZScore_Label.AutoSize = true;
+            this.ZScore_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ZScore_Label.Location = new System.Drawing.Point(21, 454);
+            this.ZScore_Label.Name = "ZScore_Label";
+            this.ZScore_Label.Size = new System.Drawing.Size(63, 16);
+            this.ZScore_Label.TabIndex = 67;
+            this.ZScore_Label.Text = "Z-Score";
+            // 
+            // District_Label
+            // 
+            this.District_Label.AutoSize = true;
+            this.District_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.District_Label.Location = new System.Drawing.Point(21, 309);
+            this.District_Label.Name = "District_Label";
+            this.District_Label.Size = new System.Drawing.Size(56, 16);
+            this.District_Label.TabIndex = 66;
+            this.District_Label.Text = "District";
+            // 
+            // AcadamicYear_Label
+            // 
+            this.AcadamicYear_Label.AutoSize = true;
+            this.AcadamicYear_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AcadamicYear_Label.Location = new System.Drawing.Point(20, 356);
+            this.AcadamicYear_Label.Name = "AcadamicYear_Label";
+            this.AcadamicYear_Label.Size = new System.Drawing.Size(114, 16);
+            this.AcadamicYear_Label.TabIndex = 65;
+            this.AcadamicYear_Label.Text = "Acadamic Year";
+            // 
+            // LastName_Label
+            // 
+            this.LastName_Label.AutoSize = true;
+            this.LastName_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LastName_Label.Location = new System.Drawing.Point(21, 262);
+            this.LastName_Label.Name = "LastName_Label";
+            this.LastName_Label.Size = new System.Drawing.Size(82, 32);
+            this.LastName_Label.TabIndex = 64;
+            this.LastName_Label.Text = "Last Name\r\n\r\n";
+            // 
+            // FirstName_Label
+            // 
+            this.FirstName_Label.AutoSize = true;
+            this.FirstName_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FirstName_Label.Location = new System.Drawing.Point(20, 215);
+            this.FirstName_Label.Name = "FirstName_Label";
+            this.FirstName_Label.Size = new System.Drawing.Size(83, 32);
+            this.FirstName_Label.TabIndex = 63;
+            this.FirstName_Label.Text = "First Name\r\n\r\n";
+            // 
+            // RagistrationNo_MaskedTextBox
+            // 
+            this.RagistrationNo_MaskedTextBox.BackColor = System.Drawing.SystemColors.Info;
+            this.RagistrationNo_MaskedTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RagistrationNo_MaskedTextBox.Location = new System.Drawing.Point(162, 163);
+            this.RagistrationNo_MaskedTextBox.Mask = "                      IM / 2000 / 00";
+            this.RagistrationNo_MaskedTextBox.Name = "RagistrationNo_MaskedTextBox";
+            this.RagistrationNo_MaskedTextBox.Size = new System.Drawing.Size(205, 22);
+            this.RagistrationNo_MaskedTextBox.TabIndex = 62;
+            // 
+            // RagistrationNo_Label
+            // 
+            this.RagistrationNo_Label.AutoSize = true;
+            this.RagistrationNo_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RagistrationNo_Label.Location = new System.Drawing.Point(20, 166);
+            this.RagistrationNo_Label.Name = "RagistrationNo_Label";
+            this.RagistrationNo_Label.Size = new System.Drawing.Size(116, 16);
+            this.RagistrationNo_Label.TabIndex = 61;
+            this.RagistrationNo_Label.Text = "Registration No";
+            // 
+            // ExtraCarricularActivities_TextBox
+            // 
+            this.ExtraCarricularActivities_TextBox.BackColor = System.Drawing.SystemColors.Info;
+            this.ExtraCarricularActivities_TextBox.Location = new System.Drawing.Point(420, 295);
+            this.ExtraCarricularActivities_TextBox.Multiline = true;
+            this.ExtraCarricularActivities_TextBox.Name = "ExtraCarricularActivities_TextBox";
+            this.ExtraCarricularActivities_TextBox.Size = new System.Drawing.Size(215, 137);
+            this.ExtraCarricularActivities_TextBox.TabIndex = 80;
+            this.ExtraCarricularActivities_TextBox.Text = "1.\r\n\r\n2.\r\n\r\n3.\r\n\r\n4.\r\n\r\n5.";
+            // 
+            // ZScore_MaskedTextBox
+            // 
+            this.ZScore_MaskedTextBox.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.ZScore_MaskedTextBox.Location = new System.Drawing.Point(162, 453);
+            this.ZScore_MaskedTextBox.Name = "ZScore_MaskedTextBox";
+            this.ZScore_MaskedTextBox.Size = new System.Drawing.Size(205, 20);
+            this.ZScore_MaskedTextBox.TabIndex = 81;
+            // 
+            // Registration_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(658, 534);
+            this.Controls.Add(this.ZScore_MaskedTextBox);
+            this.Controls.Add(this.ExtraCarricularActivities_TextBox);
+            this.Controls.Add(this.Stream_ComboBox);
+            this.Controls.Add(this.District_ComboBox);
+            this.Controls.Add(this.LastName_TextBox);
+            this.Controls.Add(this.AcadamicYear_ComboBox);
+            this.Controls.Add(this.FirstName_TextBox);
+            this.Controls.Add(this.ExtraCarricularActivities_Label);
+            this.Controls.Add(this.Profile_Label);
+            this.Controls.Add(this.Profile_PictureBox);
+            this.Controls.Add(this.Stream_Label);
+            this.Controls.Add(this.ZScore_Label);
+            this.Controls.Add(this.District_Label);
+            this.Controls.Add(this.AcadamicYear_Label);
+            this.Controls.Add(this.LastName_Label);
+            this.Controls.Add(this.FirstName_Label);
+            this.Controls.Add(this.RagistrationNo_MaskedTextBox);
+            this.Controls.Add(this.RagistrationNo_Label);
+            this.Name = "Registration_Form";
+            this.Text = "Registration_Form";
+            this.Load += new System.EventHandler(this.Registration_Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Profile_PictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox Stream_ComboBox;
+        private System.Windows.Forms.ComboBox District_ComboBox;
+        private System.Windows.Forms.TextBox LastName_TextBox;
+        private System.Windows.Forms.ComboBox AcadamicYear_ComboBox;
+        private System.Windows.Forms.TextBox FirstName_TextBox;
+        private System.Windows.Forms.Label ExtraCarricularActivities_Label;
+        private System.Windows.Forms.Label Profile_Label;
+        private System.Windows.Forms.PictureBox Profile_PictureBox;
+        private System.Windows.Forms.Label Stream_Label;
+        private System.Windows.Forms.Label ZScore_Label;
+        private System.Windows.Forms.Label District_Label;
+        private System.Windows.Forms.Label AcadamicYear_Label;
+        private System.Windows.Forms.Label LastName_Label;
+        private System.Windows.Forms.Label FirstName_Label;
+        private System.Windows.Forms.MaskedTextBox RagistrationNo_MaskedTextBox;
+        private System.Windows.Forms.Label RagistrationNo_Label;
+        private System.Windows.Forms.TextBox ExtraCarricularActivities_TextBox;
+        private System.Windows.Forms.MaskedTextBox ZScore_MaskedTextBox;
+    }
+}
